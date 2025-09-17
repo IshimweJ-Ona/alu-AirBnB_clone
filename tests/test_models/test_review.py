80c1081dede5bb130b006bb74bb20d0696aaa3d5
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-09-17 15:23:34 +0200

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+import unittest
+from models.review import Review
+from models.base_model import BaseModel
+
+
+class TestReview(unittest.TestCase):
+    """Simple tests for Review"""
+
+    def test_instance(self):
+        obj = Review()
+        self.assertIsInstance(obj, Review)
+        self.assertTrue(issubclass(Review, BaseModel))
+
+
+if __name__ == "__main__":
+    unittest.main()
