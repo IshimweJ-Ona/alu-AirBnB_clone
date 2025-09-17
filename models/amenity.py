254181d18950716b9a7de3c8aa597dfae4bc688e
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-09-17 13:54:26 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""
+Amenity module that defines the Amenity class.
+"""
+
 from models.base_model import BaseModel
+
 
 class Amenity(BaseModel):
     """Amenity class that inherits from BaseModel."""
