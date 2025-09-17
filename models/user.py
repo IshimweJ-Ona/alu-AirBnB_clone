7a6d8549d647d9f0d182157bd051dea31937815c
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-09-17 14:00:34 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""
+User module that defines the User class.
+"""
+
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """User class that inherits from BaseModel"""
+    """User class that inherits from BaseModel."""
     pass
-  
