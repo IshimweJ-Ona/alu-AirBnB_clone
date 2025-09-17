8923660c2dad9002c86923c1d767d7177ee9e98f
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-09-17 15:22:19 +0200

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+import unittest
+from models.city import City
+from models.base_model import BaseModel
+
+
+class TestCity(unittest.TestCase):
+    """Simple tests for City"""
+
+    def test_instance(self):
+        obj = City()
+        self.assertIsInstance(obj, City)
+        self.assertTrue(issubclass(City, BaseModel))
+
+
+if __name__ == "__main__":
+    unittest.main()
