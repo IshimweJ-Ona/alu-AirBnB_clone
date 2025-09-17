f5745dedea4a9541fd8424e47a7852c28530d88d
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-09-17 15:22:52 +0200

    Update test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+import unittest
+from models.place import Place
+from models.base_model import BaseModel
+
+
+class TestPlace(unittest.TestCase):
+    """Simple tests for Place"""
+
+    def test_instance(self):
+        obj = Place()
+        self.assertIsInstance(obj, Place)
+        self.assertTrue(issubclass(Place, BaseModel))
+
+
+if __name__ == "__main__":
+    unittest.main()
