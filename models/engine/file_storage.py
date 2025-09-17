057ed2403f21c5fcebdb5ea1718552ac73b07739
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-09-17 13:41:07 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+class FileStorage:
+    def __init__(self):
+        pass
+    
+    def reload(self):
+        # Implementation for reloading storage from file
+        pass
+      
