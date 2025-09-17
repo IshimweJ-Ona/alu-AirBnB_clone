a306176b28182a3157f56327848e3f619744ad3d
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-09-17 15:24:02 +0200

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+import unittest
+from models.state import State
+from models.base_model import BaseModel
+
+
+class TestState(unittest.TestCase):
+    """Simple tests for State"""
+
+    def test_instance(self):
+        obj = State()
+        self.assertIsInstance(obj, State)
+        self.assertTrue(issubclass(State, BaseModel))
+
+
+if __name__ == "__main__":
+    unittest.main()
