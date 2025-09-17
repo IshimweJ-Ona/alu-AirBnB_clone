bb5f42672cd3d7a3693824c7a44fe29d2c204d0f
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-09-17 15:24:26 +0200

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+import unittest
+from models.user import User
+from models.base_model import BaseModel
+
+
+class TestUser(unittest.TestCase):
+    """Simple tests for User"""
+
+    def test_instance(self):
+        obj = User()
+        self.assertIsInstance(obj, User)
+        self.assertTrue(issubclass(User, BaseModel))
+
+
+if __name__ == "__main__":
+    unittest.main()
