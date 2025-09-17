41e4354e2990dcc473ff50e484cd5fdac2ee03f6
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-09-17 15:21:40 +0200

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """Simple tests for BaseModel"""
+
+    def test_instance(self):
+        obj = BaseModel()
+        self.assertIsInstance(obj, BaseModel)
+
+    def test_has_id(self):
+        obj = BaseModel()
+        self.assertTrue(hasattr(obj, "id"))
+
+    def test_has_created_at(self):
+        obj = BaseModel()
+        self.assertTrue(hasattr(obj, "created_at"))
+
+    def test_has_updated_at(self):
+        obj = BaseModel()
+        self.assertTrue(hasattr(obj, "updated_at"))
+
+
+if __name__ == "__main__":
+    unittest.main()
+
