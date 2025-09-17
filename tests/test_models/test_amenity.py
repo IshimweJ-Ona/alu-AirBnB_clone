db34dc326e6d16cb4cabe65ecdcdcea50459da57
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-09-17 15:21:04 +0200

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+import unittest
+from models.amenity import Amenity
+from models.base_model import BaseModel
+
+
+class TestAmenity(unittest.TestCase):
+    """Simple tests for Amenity"""
+
+    def test_instance(self):
+        obj = Amenity()
+        self.assertIsInstance(obj, Amenity)
+        self.assertTrue(issubclass(Amenity, BaseModel))
+
+
+if __name__ == "__main__":
+    unittest.main()
