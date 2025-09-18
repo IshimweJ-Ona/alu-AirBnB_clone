ff1e8621a7595612b143dacd729baa9433d5d894
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-09-18 16:04:57 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""
-Amenity module that defines the Amenity class.
-"""
+"""Amenity module for the AirBnB clone project."""
+
 
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Amenity class that inherits from BaseModel."""
-    pass
+    """Amenity class that inherits from BaseModel, should be a string."""
+    name = ""
+    
