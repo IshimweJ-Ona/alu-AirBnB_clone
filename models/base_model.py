e6a8fd8c9242c8fa2dc1e460141050811683b339
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-09-18 16:05:19 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,48 @@
 #!/usr/bin/python3
 """
-BaseModel module defines all common attributes/methods for other classes.
+Module that defines the BaseModel class.
 """
+
+import uuid
+from datetime import datetime
 
 
 class BaseModel:
-    """Base class for all models."""
-    pass
+    """Base class for all AirBnB objects."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new instance."""
+        from models import storage  # local import to avoid circular import
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key in ("created_at", "updated_at"):
+                    if isinstance(value, str):
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Update the updated_at timestamp and save to storage."""
+        from models import storage  # local import
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """Return a dictionary representation of the instance."""
+        dict_copy = self.__dict__.copy()
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        return dict_copy
