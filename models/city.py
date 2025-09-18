59333ef5170a0f364d6e4ad0c289e0dc58c531fd
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-09-18 16:05:45 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""
-City module that defines city class
-"""
+"""City class for AirBnB project."""
 
 
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """City class that inherits from BaseModel"""
-    pass
+    """City class that inherits from BaseModel."""
+
+    state_id = ""
+    name = ""
+    
