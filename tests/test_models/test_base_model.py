07587c637c7a96f7deb54e765b27b104c59050bf
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-09-18 16:09:29 +0200

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+Unit tests for the basemodel class.
+"""
+
+
 import unittest
+from datetime import datetime
 from models.base_model import BaseModel
 
 
@@ -13,16 +19,52 @@ class TestBaseModel(unittest.TestCase):
     def test_has_id(self):
         obj = BaseModel()
         self.assertTrue(hasattr(obj, "id"))
+        self.assertIsInstance(obj.id, str)
 
-    def test_has_created_at(self):
+    def test_created_and_updated_are_datetime(self):
         obj = BaseModel()
-        self.assertTrue(hasattr(obj, "created_at"))
+        self.assertIsInstance(obj.created_at, datetime)
+        self.assertIsInstance(obj.updated_at, datetime)
 
-    def test_has_updated_at(self):
+    def _test_str_method(self):
         obj = BaseModel()
-        self.assertTrue(hasattr(obj, "updated_at"))
+        string = str(obj)
+        self.assertIn("[BaseModel]", string)
+        self.assertIn(obj.id, string)
+
+    def test_save_updates_updated_at(self):
+        obj = BaseModel()
+        old_updated_at = obj.updated_at
+        obj.save()
+        self.assertNotEqual(old_updated_at, obj.updated_at)
+
+    def test_to_dict_has_correct_keys(self):
+        obj = BaseModel()
+        d = obj.to_dict()
+        self.assertIsInstance(d["created_at"], str)
+        self.assertIsInstance(d["updated_at"], str)
+        datetime.fromisoformat(d["created_at"])
+        datetime.fromisoformat(d["updated_at"])
+        self.assertEqual(d["__class__"], "BaseModel")
+
+    def test_init_from_kwargs(self):
+        """Test if BaseModel can be reached from a dict (kwargs)."""
+        obj = BaseModel()
+        obj.name = "Test"
+        obj.number = 123
+        obj_dict = obj.to_dict()
+
+        new_obj = BaseModel(**obj_dict)
+
+        #check IDs match
+        self.assertEqual(obj.id, new_obj.id)
+        #created_at and updated_at 
+        self.assertIsInstance(new_obj.created_at, datetime)
+        self.assertIsInstance(new_obj.updated_at, datetime)
+        #Extra attributes
+        self.assertEqual(new_obj.name, "Test")
+        self.assertEqual(new_obj.number, 123)
 
 
 if __name__ == "__main__":
     unittest.main()
-
