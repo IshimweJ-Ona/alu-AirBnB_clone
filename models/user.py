5daf5d0c06f85c4a95bccb217886eef4cb2825fd
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-09-18 16:07:18 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """
-User module that defines the User class.
+User class for AirBnB clone.
 """
 
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """User class that inherits from BaseModel."""
-    pass
+    """User Public class inherits from BaseModel."""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
