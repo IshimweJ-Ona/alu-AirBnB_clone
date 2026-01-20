fee25821a9c628787c163e1c1b512141daed8e08
IshimweJ-Ona <j.ishimwe4@alustudent.com>
2026-01-20 18:47:25 +0200

    Command interpreter for the whole system

--- console.py
+++ console.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python3
 """"Command interpreter for the AirBnB clone project."""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for System."""
+
+    prompt= "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+    
+    def do_EOF(self, arg):
+        """Exit the program with Ctrl-D (EOF)"""
+        print()  # Add a new line foe clean output
+        return True
+    
+    def emptyline(self):
+        """Do nothing on empty input line"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
