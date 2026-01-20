586caac19ea5175d309531d62b3e0abec4678bb6
IshimweJ-Ona <j.ishimwe4@alustudent.com>
2026-01-20 16:02:26 +0200

    new additions to files

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,44 @@
 #!/usr/bin/python3
-"""Defines the BasModel class."""
+"""
+BasModel class defines all common attributes/methods for other classes.
+Public attributes:{"id(uuid.uuid4)", "created_at", "updated_at"},
+Public instance method: {"save(self)", "to_dict"}
+"""
+
+
+import uuid
+from datetime import datetime
 
 
 class BaseModel:
-    """A simple placeholder for the BaseModel model."""
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    self.created_at = datetime.fromisoformat(value)
+                elif key == "updated_at":
+                    self.updated_at = datetime.fromisoformat(value)
+                elif key != "__class__":
+                    setattr(self, key, value)
 
-    def __init__(self):
-        """Initialize BaseModel instance."""
-        pass
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
+    def __str__(self):
+        """Return the string representation of the instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    
+    def save(self):
+        """Update the updated_at attribute with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary presentation of the instance."""
+        return {
+            **self.__dict__,
+            "__class__": self.__class__.__name__,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
+        }
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,15 +4,68 @@
 
 import unittest
 from models.base_model import BaseModel
+from datetime import datetime
 
 
 class TestBaseModel(unittest.TestCase):
     """test cases for the BaseModel class."""
 
-    def test_instance(self):
+    def test_instance_creation(self):
         """Test that a BaseModel instance can be created."""
         instance = BaseModel()
         self.assertIsNotNone(instance)
+
+    def test_attributes_exist(self):
+        """Test that id, created_at and updated_at exxist."""
+        instance = BaseModel()
+        self.assertTrue(hasattr(instance, "id"))
+        self.assertTrue(hasattr(instance, "created_at"))
+        self.assertTrue(hasattr(instance, "updated_at"))
+
+    def test_id_is_string(self):
+        instance = BaseModel()
+        self.assertIsInstance(instance.created_at, datetime)
+
+    def test_updated_at_is_datetime(self):
+        instance = BaseModel()
+        self.assertIsInstance(instance.updated_at, datetime)
+
+    def test_str_method(self):
+        """Test representation format."""
+        instance = BaseModel()
+        string = str(instance)
+        self.assertIn(instance.id, string)
+        self.assertIn("BaseModel", string)
+
+    def test_save_updates_updated_at(self):
+        instance = BaseModel()
+        old_time = instance.updated_at
+        instance.save()
+        self.assertNotEqual(old_time, instance.updated_at)
+
+    def test_to_dict_returns_dict(self):
+        """Test that to_dict returns a dictionary."""
+        instance = BaseModel()
+        self.assertIsInstance(instance.to_dict(), dict)
+
+    def test_to_dict_contains_class(self):
+        instance = BaseModel()
+        self.assertIn("__class__", instance.to_dict())
+
+    def test_to_dict_datetime_are_strings(self):
+        instance = BaseModel()
+        dictionary = instance.to_dict()
+        self.assertIsInstance(dictionary["created_at"], str)
+        self.assertIsInstance(dictionary["updated_at"], str)
+
+    def test_kwargs_reconstruction(self):
+        """Test that instance can be reconstructed using kwargs."""
+        instance = BaseModel()
+        dictionary = instance.to_dict()
+        new_instance = BaseModel(**dictionary)
+        self.assertEqual(instance.id, new_instance.id)
+        self.assertEqual(instance.created_at, new_instance.created_at)
+        self.assertEqual(instance.updated_at, new_instance.updated_at)
 
 
 if __name__ == "__main__":
