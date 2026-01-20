f1383c80d8c3be0e44ef2978c8647a6e72f59424
IshimweJ-Ona <j.ishimwe4@alustudent.com>
2026-01-20 19:38:02 +0200

    Update cmd for the system

--- console.py
+++ console.py
@@ -1,55 +1,55 @@
 #!/usr/bin/python3
-""""Command interpreter for the AirBnB clone project."""
+"""Command interpreter for the AirBnB clone project."""
 
 import cmd
 from models import storage
 from models.base_model import BaseModel
 
-
+# Mapping class names to actual classes
 classes = {"BaseModel": BaseModel}
 
 
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter for System."""
+    """Command interpreter for the AirBnB clone system."""
 
-    prompt= "(hbnb) "
+    prompt = "(hbnb) "
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
-    
+
     def do_EOF(self, arg):
         """Exit the program with Ctrl-D (EOF)"""
-        print()  # Add a new line foe clean output
+        print()  # Clean new line
         return True
-    
+
     def emptyline(self):
         """Do nothing on empty input line"""
         pass
 
     def do_create(self, arg):
-        """Create a new instance of a class and prints its id"""
+        """Create a new instance of a class and print its id"""
         if not arg:
             print("** class name missing **")
             return
         if arg not in classes:
-            print(" class doesn't exist **")
+            print("** class doesn't exist **")
             return
         instance = classes[arg]()
         storage.save()
         print(instance.id)
 
     def do_show(self, arg):
-        """Print instance representation"""
+        """Print the string representation of an instance"""
         args = arg.split()
         if len(args) == 0:
-            print("** class name  missing **")
+            print("** class name missing **")
             return
         if args[0] not in classes:
             print("** class doesn't exist **")
             return
         if len(args) == 1:
-            print("** instance id is missing **")
+            print("** instance id missing **")
             return
         key = f"{args[0]}.{args[1]}"
         all_objs = storage.all()
@@ -68,7 +68,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
             return
         if len(args) == 1:
-            print("** instance id is missing **")
+            print("** instance id missing **")
             return
         key = f"{args[0]}.{args[1]}"
         all_objs = storage.all()
@@ -79,16 +79,15 @@ class HBNBCommand(cmd.Cmd):
         storage.save()
 
     def do_all(self, arg):
-        """Print all string instances"""
+        """Print all string representations of instances"""
         all_objs = storage.all()
         if arg:
             if arg not in classes:
                 print("** class doesn't exist **")
                 return
-            result = [str(obj) for k, obj in all_objs.items()
-                      if k.startswith(arg + ".")]
+            result = [str(obj) for k, obj in all_objs.items() if k.startswith(arg + ".")]
         else:
-            result = [str(obj) for k, obj in all_objs.values()]
+            result = [str(obj) for obj in all_objs.values()]
         print(result)
 
     def do_update(self, arg):
@@ -98,10 +97,10 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
             return
         if args[0] not in classes:
-            print("** clas doesn't exist **")
+            print("** class doesn't exist **")
             return
         if len(args) == 1:
-            print("** instance id is missing **")
+            print("** instance id missing **")
             return
         key = f"{args[0]}.{args[1]}"
         all_objs = storage.all()
@@ -114,12 +113,12 @@ class HBNBCommand(cmd.Cmd):
         if len(args) == 3:
             print("** value missing **")
             return
-        
+
         obj = all_objs[key]
         attr_name = args[2]
         attr_value = args[3]
 
-        # Cast value correct type if possible
+        # Cast value to correct type if possible
         try:
             if attr_value.isdigit():
                 attr_value = int(attr_value)
@@ -134,9 +133,10 @@ class HBNBCommand(cmd.Cmd):
         # Do not update protected attributes
         if attr_name in ("id", "created_at", "updated_at"):
             return
-        
+
         setattr(obj, attr_name, attr_value)
         storage.save()
-        
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
